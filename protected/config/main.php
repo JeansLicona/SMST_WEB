d06d6c32e424d958151810af31092c63f240f5c6
Jeans Michell <licona.dorantes@gmail.com>
2014-02-01 19:43:20 -0600

    Redirección una vez logeado
    Una vez que inicie sesión te dirige a otra pagina diferente de login

--- protected/config/main.php
+++ protected/config/main.php
@@ -11,7 +11,7 @@ return array(
         
         'language'=>'es',
         'theme'=>'blackboot',
-        //'defaultController' => 'Site/login',
+        'defaultController' => 'Site/login',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
