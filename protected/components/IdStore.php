ce7573b29c70a3b6f97a049db60a0300074c5569
bzeheatnix <heatheal@gmail.com>
2014-05-17 12:33:31 -0500

    Creacion IdStore.php en components

---
+++ protected/components/IdStore.php
@@ -0,0 +1,17 @@
+<?php
+
+    /**
+     * UserIdentity represents the data needed to identity a user.
+     * It contains the authentication method that checks if the provided
+     * data can identity the user.
+     */
+    class IdStore extends CIdStore {
+
+        /**
+         * Store registration ids
+         */
+        public function store() {
+
+        }
+
+    }
